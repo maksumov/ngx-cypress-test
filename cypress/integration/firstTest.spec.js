90760508fe3aa90ed6bdf7998444d53e9ad38976
Marat Maksumov <maxumov@gmail.com>
2020-11-27 22:18:38 +0300

    feat: add test with different types of locators

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,5 +1,36 @@
 /// <reference types="cypress" />
 
-describe("Our First Suite", () => {
-  it("first test", () => {});
+describe('Our First Suite', () => {
+  it('finds element', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    // By Tag Name
+    cy.get('input');
+
+    // By Id
+    cy.get('#inputEmail1');
+
+    // By Class Name
+    cy.get('.input-full-width');
+
+    // By Attribute Name
+    cy.get('[placeholder="Email"]');
+
+    // By Class Value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
+
+    // By Tag Name and Attribute With Value
+    cy.get('input[placeholder="Email"]');
+
+    // By Two Different Attributes
+    cy.get('[placeholder="Email"][fullwidth]');
+
+    // By Tag Name, Attribute with Value, ID and Class Name
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
+
+    // The most recommended way by Cypress (creating your own attributes)
+    cy.get('[data-cy="imputEmail1"]');
+  });
 });
