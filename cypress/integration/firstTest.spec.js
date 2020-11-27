0fa364abc630d8fca506043f6f49fef02fd8f9e8
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-28 01:59:14 +0300

    feat: add second test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -33,4 +33,28 @@ describe('Our First Suite', () => {
     // The most recommended way by Cypress (creating your own attributes)
     cy.get('[data-cy="imputEmail1"]');
   });
+
+  it.only('second test', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    cy.get('[data-cy="signInButton"]');
+
+    // Find first element that contains 'Sign in'
+    cy.contains('Sign in');
+
+    // Find element (second sign in button) containing 'Sign in' by its status
+    cy.contains('[status="warning"]', 'Sign in');
+
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click();
+
+    cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
+  });
 });
