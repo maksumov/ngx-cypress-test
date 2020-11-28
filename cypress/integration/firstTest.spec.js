242421087821c00e3e9746fb072bc98676422e4e
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-28 18:48:14 +0300

    feat: add test with Cypress & JQuery contexts

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -34,7 +34,7 @@ describe('Our First Suite', () => {
     cy.get('[data-cy="imputEmail1"]');
   });
 
-  it.only('second test', () => {
+  it('second test', () => {
     cy.visit('/');
     cy.contains('Forms').click();
     cy.contains('Form Layouts').click();
@@ -56,5 +56,65 @@ describe('Our First Suite', () => {
       .click();
 
     cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
+  });
+
+  it.only('then and wrap methods', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    // Original approach
+    // cy.contains('nb-card', 'Using the Grid')
+    //   .find('[for="inputEmail1"]')
+    //   .should('contain', 'Email');
+    // cy.contains('nb-card', 'Using the Grid')
+    //   .find('[for="inputPassword2"]')
+    //   .should('contain', 'Password');
+
+    // cy.contains('nb-card', 'Basic form')
+    //   .find('[for="exampleInputEmail1"]')
+    //   .should('contain', 'Email address');
+    // cy.contains('nb-card', 'Basic form')
+    //   .find('[for="exampleInputPassword1"]')
+    //   .should('contain', 'Password');
+
+    // Selenium way (doesn't work)
+    // const firstForm = cy.contains('nb-card', 'Using the Grid');
+    // const secondForm = cy.contains('nb-card', 'Basic form');
+
+    // firstForm.find('[for="inputEmail1"]').should('contain', 'Email');
+    // firstForm.find('[for="inputPassword2"]').should('contain', 'Password');
+    // secondForm
+    //   .find('[for="exampleInputEmail1"]')
+    //   .should('contain', 'Email address');
+    // secondForm
+    //   .find('[for="exampleInputPassword1"]')
+    //   .should('contain', 'Password');
+
+    // Cypress style
+    cy.contains('nb-card', 'Using the Grid').then((firstForm) => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+      const passwordLabelFirst = firstForm
+        .find('[for="inputPassword2"]')
+        .text();
+
+      expect(emailLabelFirst).to.equal('Email');
+      expect(passwordLabelFirst).to.equal('Password');
+
+      cy.contains('nb-card', 'Basic form').then((secondForm) => {
+        // secondForm is JQuery object
+        const passwordLabelSecond = secondForm
+          .find('[for="exampleInputPassword1"]')
+          .text();
+        // so we should use assertions with expect (Chai style)
+        expect(passwordLabelSecond).to.equal(passwordLabelFirst);
+
+        // cy.wrap() method let use Cypress.Chainable methods with JQuery objects
+        // so we could use .should()
+        cy.wrap(secondForm)
+          .find('[for="exampleInputPassword1"]')
+          .should('contain', 'Password');
+      });
+    });
   });
 });
